021a40ca22fc36b9b3cc9c2ea229bdbc7270f5e1
Alumno <Alumno@LAB1504-28>
2023-11-30 21:54:26 -0500

    Agregar archivos de proyecto.

---
+++ API_Lab14_Guerrero/Models/Detail.cs
@@ -0,0 +1,16 @@
+﻿namespace API_Lab14_Guerrero.Models
+{
+    public class Detail
+    {
+        public int DetailID { get; set; }
+        public int Amount { get; set; }
+        public float Price { get; set; }
+        public float SubTotal { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
+    }
+}
---
+++ API_Lab14_Guerrero/Models/Invoice.cs
@@ -0,0 +1,13 @@
+﻿namespace API_Lab14_Guerrero.Models
+{
+    public class Invoice
+    {
+        public int InvoiceID { get; set; }
+        public DateTime Date { get; set; }
+        public string InvoiceNumber { get; set; }
+        public float Total { get; set;}
+
+        public int? CustomerId { get; set; }
+        public Customer? Customer { get; set; }
+    }
+}
